f406864155677b4651ffb743fc59fc1f173a580f
Your Name <you@example.com>
2025-06-23 16:27:40 +0800

    2025062302

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ App({
         // env 参数说明：
         // env 参数决定接下来小程序发起的云开发调用会默认请求到哪个云环境的资源
         // 此处请填入环境 ID, 环境 ID 可打开云控制台查看
-        //env: 'digital-patrol-cloud', // 默认环境，可修改为你的云环境ID
         env: 'digital1-3gib77atb7c77aad', // 默认环境，可修改为你的云环境ID
         traceUser: true,
       })
